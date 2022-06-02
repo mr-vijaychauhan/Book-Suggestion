3d739806c476e850667988d90a6bb5f2658230ed
VIJAY CHAUHAN <mr.vijaychauhan08@gmail.com>
2022-06-03 00:26:16 +0530

    Recommended Books with Book Detail

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # save this as app.py
 from flask import Flask,render_template
 import pickle
+from slugify import slugify
+import numpy as np
 
-popular_df= pickle.load(open('popular.pkl','rb'))
+
+## Model 
+popular_df= pickle.load(open('model/popular.pkl','rb'))
+pt= pickle.load(open('model/pt.pkl','rb'))
+books= pickle.load(open('model/books.pkl','rb'))
+similarity_score= pickle.load(open('model/similarity_score.pkl','rb'))
 
 app = Flask(__name__,static_url_path='/static')
 
@@ -16,6 +23,30 @@ def index():
         ratings= list(popular_df['avg_ratings'].values),
         year_of_publication= list(popular_df['Year-Of-Publication'].values),
     )
+
+@app.route("/book/<path:bookname>")
+def book(bookname):
+    index= np.where(pt.index==bookname)[0][0]
+    similar_items= sorted(list(enumerate(similarity_score[index])),key=lambda x:x[1],reverse=True)[1:11]
+    data= [] 
+    for i in similar_items:
+        item = []
+        temp_df=books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Year-Of-Publication'].values))
+        data.append(item)
+
+        books_details = [] 
+        book_items= []
+        book_items.extend(list(books[books['Book-Title'] == bookname].drop_duplicates('Book-Title')['Book-Title'].values))
+        book_items.extend(list(books[books['Book-Title'] == bookname].drop_duplicates('Book-Title')['Book-Author'].values))
+        book_items.extend(list(books[books['Book-Title'] == bookname].drop_duplicates('Book-Title')['Image-URL-M'].values))
+        book_items.extend(list(books[books['Book-Title'] == bookname].drop_duplicates('Book-Title')['Year-Of-Publication'].values))
+        books_details.append(book_items)
+
+    return render_template('book-recommeded.html',data= data,books_details=books_details)
 
 if __name__ == '__main__':
     app.run(debug=True)
